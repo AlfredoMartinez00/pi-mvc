8d5b39550680a9957ac389a83e8c3e0fdcd4fd99
AlfredoMartinez00 <mgjo141686@upemor.edu.mx>
2017-05-15 08:44:51 -0500

    Errores corregidos en la vista de productos

--- view/products/index.php
+++ view/products/index.php
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title></title>
-	</head>
-	<body>
-		<?php
-		foreach ($products as $product)
-		{
-		?>
-			<div class="">
-				<h3><?php echo $product->name?></h3>
-			</div>
-		<?php
-		}
-		?>
-	</body>
-</html>
+
+			<?php
+			foreach ($products as $product)
+			{
+			?>
+				<div class="">
+					<h3><?php echo $product->name?></h3>
+				</div>
+			<?php
+			}
+			?>
